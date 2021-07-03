7144a8c47889ee89fc88e1f393c82aa1c5f660ef
Mark Karpeles <magicaltux@gmail.com>
2021-07-04 02:37:18 +0900

    adding examples - cobra is pulling a whole lot of crap

---
+++ streamdeck-examples/cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "",
+	Short: "This is a demo application for the golang streamdeck driver",
+	Long:  `This is a demo application for the golang streamdeck driver`,
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// cobra.OnInitialize(initConfig)
+}
---
+++ streamdeck-examples/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/KarpelesLab/streamdeck/streamdeck-examples/cmd"
+
+func main() {
+	cmd.Execute()
+}
